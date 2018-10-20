7918e03862aab9207ea2133fc40ce585862a9133
Luiz Eduardo <servergta@gmail.com>
2018-10-20 04:58:20 -0300

    Add seeding

--- api/database/seeds/DatabaseSeeder.php
+++ api/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('urls')->insert([
+            'url' => "https://www.google.com.br/search?q=audima&safe=off&cad=h"
+        ]);
     }
 }
